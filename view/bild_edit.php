4f2a61f9e30168933d42369bb4c40f0f17fdb9af
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-05-24 11:10:54 +0200

    views
    test

---
+++ view/bild_edit.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vmadmin
+ * Date: 18.05.2018
+ * Time: 08:40
+ */
+$lblClass = "col-md-2";
+$eltClass = "col-md-4";
+$btnClass = "btn btn-success";
+
+$form = new Form($GLOBALS['appurl']."/bild/edit/" . $gid);
+$button = new ButtonBuilder();
+
+
+echo $form->input()->label('Bild Name:')->name('name')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $form->input()->label('Bezeichnung')->name('bez')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $form->input()->label('Bild:')->name('pic')->type('file')->lblClass($lblClass)->eltClass($eltClass);
+
+if($gid == 0) {
+    echo $form->input()->label('Gallerie')->name('gid')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+}else {
+    echo $form->input()->name('gid')->type('hidden')->value($gid);
+}
+// echo $form->select()->label('Public')->name('isPublic')
+echo $button->start($lblClass, $eltClass);
+echo $button->label('Add')->name('sendBild')->type('submit')->class('btn-success');
+echo $button->end();
+echo $form->end();
+if($err !== null ) {
+    echo "<p class='errMsg'>" . $err . "</p>";
+}
--- view/galerie_show.php
+++ view/galerie_show.php
@@ -6,5 +6,15 @@
  * Time: 10:13
  */
 $button = new ButtonBuilder();
-echo $button->label('Bild hinzufügen')->name('bild Hinzufügen')->type('link')->class('btn-success')->link("/galerie");
+echo $button->label('Bild hinzufügen')->name('bild Hinzufügen')->type('link')->class('btn-success')->link("/bild/edit/{$id}");
+echo $button->label('Gallerie Löschen')->name('Gallerie Löschen')->type('link')->class('btn-success')->link("/galerie/delete/{$id}");
+echo "<br/>";
 echo $beschreibung;
+echo "<br/>";
+for($i = 0; $i < sizeof($bilder);$i++){
+    echo "<a href='". $GLOBALS['appurl'] . "/" . $path . "/" . $bilder[$i]->bname . "' >";
+    echo "<img src='". $GLOBALS['appurl'] . "/" . $path . "/" . $bilder[$i]->bname . "'/>";
+    echo "<h4>" . $bilder[$i]->bezeichnung . "</h4>";
+    echo "</a>";
+
+}
